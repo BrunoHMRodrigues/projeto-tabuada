e4c116c22476bab09a220629eec72dd1a00cef08
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-03 10:00:12 -0300

    Realizado lógica de respostas

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,13 @@
-let arrayAnswer = [];
+let keepArrayAnswer = [];
 
 function gerateTabuada () {
     const numberTabuada = Math.trunc(Math.random() *10)+1;
     const arrayMultiplier = [1,2,3,4,5,6,7,8,9];
     const ulPosition = document.getElementById("container-resume-tabuada");   
     const firstCalculation = document.getElementById("current-calculation");
+    let arrayAnswer = [];
+
+    
 
     for (let index = 0; index < arrayMultiplier.length; index+=1) {
         const li = document.createElement("li");
@@ -15,14 +18,45 @@ function gerateTabuada () {
 
         ulPosition.appendChild(li);  
         
-        arrayAnswer.push = numberTabuada * arrayMultiplier[index];
-    }
+        arrayAnswer[index] = numberTabuada * arrayMultiplier[index];      
+    }    
     firstCalculation.innerText = numberTabuada + " x " + arrayMultiplier[0] + " = ?";
+    
+    keepArrayAnswer = arrayAnswer;
 }
 gerateTabuada();
 
-function insertCurrentProblem () {
+let arrayNumberAnswer = 0;
 
+
+const buttonAnswer = document.getElementById("button-verify");
+let numberProblem = 0;
+
+function verifyAnswer () {    
+    const inputContainer = document.getElementById("answer-container");
+    const answer = inputContainer.value;
+    const liList = document.getElementById("container-resume-tabuada").children; 
+    const currentProblem = document.getElementById("current-calculation");
+
+    if (keepArrayAnswer[numberProblem] == answer) {
+        alert ("Parabéns!!!")
+        liList[numberProblem].innerText += " " + answer;
+
+        numberProblem += 1;
+
+        currentProblem.innerText = liList[numberProblem].innerText;
+        inputContainer.value = "";
+    } else {
+        alert("A resposta está errada");
+    }
+
+    return numberProblem;
 }
+buttonAnswer.addEventListener("click", verifyAnswer);
 
-let arrayNumberAnswer = [];
+function verifyEnter (event) {
+    if (event.key === "Enter") {
+        verifyAnswer();
+    }
+}
+document.getElementById("answer-container").addEventListener("keypress", verifyEnter);
