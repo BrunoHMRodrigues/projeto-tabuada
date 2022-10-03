2886e0333d612e7e8ee049bf978a609d01bb1762
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-03 01:09:33 -0300

    Alterações de estilo

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,10 @@
+let arrayAnswer = [];
+
 function gerateTabuada () {
     const numberTabuada = Math.trunc(Math.random() *10)+1;
     const arrayMultiplier = [1,2,3,4,5,6,7,8,9];
     const ulPosition = document.getElementById("container-resume-tabuada");   
+    const firstCalculation = document.getElementById("current-calculation");
 
     for (let index = 0; index < arrayMultiplier.length; index+=1) {
         const li = document.createElement("li");
@@ -10,7 +13,16 @@ function gerateTabuada () {
         li.className = "item-list-tabuada";
         li.innerText = problemTabuada;
 
-        ulPosition.appendChild(li);        
+        ulPosition.appendChild(li);  
+        
+        arrayAnswer.push = numberTabuada * arrayMultiplier[index];
     }
+    firstCalculation.innerText = numberTabuada + " x " + arrayMultiplier[0] + " = ?";
 }
 gerateTabuada();
+
+function insertCurrentProblem () {
+
+}
+
+let arrayNumberAnswer = [];
